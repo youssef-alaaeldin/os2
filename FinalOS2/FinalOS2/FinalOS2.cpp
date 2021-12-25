360d5744bd39d0a3c38114e0d255327bc30b7bf2
yousufalaa10 <63983084+yousufalaa10@users.noreply.github.com>
2021-12-25 14:52:54 +0200

    Optimal
    Optimal

--- FinalOS2/FinalOS2/FinalOS2.cpp
+++ FinalOS2/FinalOS2/FinalOS2.cpp
@@ -102,11 +102,100 @@ void LRU() {
     cout << "Page faults = " << pageFault;
 
 }
+void optimal() {
+
+    int  temp[10], flag1, flag2, flag3, pos, max;
+    cout<<"Enter number of frames: ";
+    cin>> noOfFrames;
+
+    cout<<"Enter number of pages: ";
+    cin >> nopages;
+
+    cout << "Enter page reference string: ";
+
+    for (i = 0; i < nopages; ++i) {
+        cin>>refString[i];
+    }
+
+    for (i = 0; i < noOfFrames; ++i) {
+        frame[i] = -1;
+    }
+
+    for (i = 0; i < nopages; ++i) {
+        flag1 = flag2 = 0;
+
+        for (j = 0; j < noOfFrames; ++j) {
+            if (frame[j] == refString[i]) {
+                flag1 = flag2 = 1;
+                break;
+            }
+        }
+
+        if (flag1 == 0) {
+            for (j = 0; j < noOfFrames; ++j) {
+                if (frame[j] == -1) {
+                    pageFault++;
+                    frame[j] = refString[i];
+                    flag2 = 1;
+                    break;
+                }
+            }
+        }
+
+        if (flag2 == 0) {
+            flag3 = 0;
+
+            for (j = 0; j < noOfFrames; ++j) {
+                temp[j] = -1;
+
+                for (k = i + 1; k < nopages; ++k) {
+                    if (frame[j] == refString[k]) {
+                        temp[j] = k;
+                        break;
+                    }
+                }
+            }
+
+            for (j = 0; j < noOfFrames; ++j) {
+                if (temp[j] == -1) {
+                    pos = j;
+                    flag3 = 1;
+                    break;
+                }
+            }
+
+            if (flag3 == 0) {
+                max = temp[0];
+                pos = 0;
+
+                for (j = 1; j < noOfFrames; ++j) {
+                    if (temp[j] > max) {
+                        max = temp[j];
+                        pos = j;
+                    }
+                }
+            }
+            frame[pos] = refString[i];
+            pageFault++;
+        }
+
+
+
+        
+    }
+
+    cout << "String after : \n";
+    for (k = 0; k < noOfFrames; k++)
+        cout << frame[k] << " ";
+    cout << endl;
+    cout << "Page faults = " << pageFault;
+
+}
 int main()
 {
     char choice = ' ';
 
-    cout << "Enter 'f' for fifo, 'l' for lfu , 'o' for optimal" << endl;
+    cout << "Enter 'f' for fifo, 'l' for LRU , 'o' for optimal" << endl;
     cin >> choice;
     switch (choice) {
     case 'f': 
@@ -115,7 +204,9 @@ int main()
     case 'l':
         LRU();
         break;
-
+    case 'o':
+        optimal();
+        break;
 
     }
 
